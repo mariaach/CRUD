e75ecc61f90cb0fc1f8a90b882393f0f936dd9f3
mariaach <cardenashenaomaria@gmail.com>
2023-04-11 21:22:40 -0500

    funcionalidad para Nuevo cliente

--- service/client-service.js
+++ service/client-service.js
@@ -2,6 +2,18 @@ const listaClientes = () =>
 fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil",{
+    method: "POST",
+    headers: {
+      "content-Type": "application/json"
+    },
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
+  });
+}
+
+
 export const clientServices = {
-  listaClientes
+  listaClientes,
+  crearCliente,
 };
