4cc67ac602fe15353bbe63c60694feb06ced6949
mariaach <cardenashenaomaria@gmail.com>
2023-04-12 21:52:30 -0500

    eliminar clientes

--- service/client-service.js
+++ service/client-service.js
@@ -1,19 +1,24 @@
-const listaClientes = () => 
-fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
-
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 const crearCliente = (nombre, email) => {
-  return fetch("http://localhost:3000/perfil",{
+  return fetch("http://localhost:3000/perfil", {
     method: "POST",
     headers: {
-      "content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ nombre, email, id: uuid.v4() }),
   });
-}
+};
 
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
+  });
+};
 
 export const clientServices = {
   listaClientes,
   crearCliente,
+  eliminarCliente,
 };
